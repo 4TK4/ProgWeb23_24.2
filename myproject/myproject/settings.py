85728136a36722c761a20ff83e5d4a61b4b56a48
Lorenzo Colombo <l.colombo34@studenti.unibg.it>
2024-07-21 12:41:11 +0200

    import style.css; table scrollable

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -121,12 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #per sapere dove cercare i file statici
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    BASE_DIR / 'static',
 ]
+
+# Per la raccolta dei file statici in produzione
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
