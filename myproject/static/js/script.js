885211e07c2b155bc13ffa2f838a43ce2fb26ba3
PoloniLuca <l.poloni4@studenti.unibg.it>
2024-07-22 10:14:10 +0200

    eliminazione ok, manca css

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -10,21 +10,3 @@ function setEliminazione(numero) {
   
     elimButton.addEventListener('click', handleElimination);
   }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    var form = document.getElementById("searchForm");
-    var resetButton = document.getElementById("reset");
-  
-    resetButton.addEventListener("click", function () {
-  
-      var inputs = form.querySelectorAll('input[type="number"], input[type="date"], input[type="text"], input[type="checkbox"]');
-      inputs.forEach(function (input) {
-        input.value = "";
-      });
-  
-      var checkboxes = form.querySelectorAll('input[type="checkbox"]');
-      checkboxes.forEach(function (checkbox) {
-        checkbox.checked = false;
-      });
-    });
-  });
